56cf2ff8a4a893dc633cc26dafd75cc32e2e5544
Matthew Chick <c.chick@unsw.edu.au>
2019-10-14 10:08:29 +1100

    how to export and create Action creator

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// use export default selectSong, we use import selectSong from '../actions
+import { selectSong } from '../actions';
 
 const App = () => {
     return <div>App</div>;
